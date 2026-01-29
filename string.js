a139fb496b554ddc7f7afb0512e0f6d6da94ba75
Al Amin <alamin.dev01@gmail.com>
2026-01-29 22:04:46 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,31 @@
+const country = 'Bangladesh';
+
+const division = "New khale"
+
+const district = `B-Baria`
+
+const thana = new String('Demra')
+
+// console.log(typeof country)
+// console.log(typeof division)
+// console.log(typeof district)
+// console.log(thana)
+
+const numbers = [12, 34, 56, 78, 12, 23,]
+console.log(numbers)
+numbers[0] = 20;
+// console.log(numbers.length)
+// console.log(numbers[3])
+console.log(numbers)
+
+
+
+// string is immutable ---------> not changeable 
+// array is mutable  ------------------> you can change the array elements
+const capital = 'Dhaka';
+// console.log(capital)
+// console.log(capital.length)
+// console.log(capital[0])
+console.log(capital)
+capital[0] = 'F'
+console.log(capital)
