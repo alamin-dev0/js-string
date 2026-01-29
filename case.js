897cadeda39912cd35cbb1b18fb25a495b4b5839
Al Amin <alamin.dev01@gmail.com>
2026-01-30 00:46:10 +0600

    added some code

---
+++ case.js
@@ -0,0 +1,17 @@
+const school = 'RAJ UK Uttara Model School'
+// console.log(school)
+// console.log(school.toLowerCase())
+// console.log(school.toUpperCase())
+
+// UPPEARCASE : ABCD
+// lowearcase :: abcd
+
+const subject = 'Chemistry';
+const book = 'chemisTry'
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+    console.log('Ei bar porikkhai fatai felbo ')
+}
+else {
+    console.log('hudai hudai pristha oltai')
+}
